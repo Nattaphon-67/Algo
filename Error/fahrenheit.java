1bd74534ed7fad0e73f04bc6370801d34affa0a7
Nattaphon-67 <naruapon@gmail.com>
2024-07-30 11:45:07 +0700

    Nattaphon45

---
+++ Error/fahrenheit.java
@@ -0,0 +1,14 @@
+package Error;
+
+public class fahrenheit {
+    public static void main (String args[]){
+        double fahrenheit = 80;
+        double celsius = (5/9) * (fahrenheit-32);
+        System.out.println(fahrenheit);
+        System.out.println(celsius);
+
+
+
+
+    }
+}
