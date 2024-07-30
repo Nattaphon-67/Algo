19ceb45755e6dce525f28bfcf1713101dcff15c4
Nattaphon-67 <naruapon@gmail.com>
2024-07-30 10:27:27 +0700

    Nattaphon

---
+++ Error/Avg.java
@@ -0,0 +1,13 @@
+package Error;
+
+public class Avg {
+public static void main (String args[]){
+
+    byte num1 = 7;
+    byte num2 = 8;
+    double sum = (double) (num1 + num2) / 2;
+    System.out.println(sum);
+
+
+}
+}
---
+++ Error/TestVariable.java
@@ -0,0 +1,43 @@
+package Error;
+
+public class TestVariable {
+    public static void main(String[] args) {
+        //1
+        char clothingSize = 's'; //สามารถเป็นตัวเเปล 's' , 'm' or '1'
+        System.out.println("ขนาดเสื้อตัวเเปร clothingSize = " +clothingSize);
+        //2
+
+        int visitor = 'v';
+        System.out.println("ขนาดตัวเเปร visitor = " +visitor);
+        //3
+
+        char grade1 = 'A';  // ตัวแปร char สำหรับเกรด A
+        char grade2 = 'B';  // ตัวแปร char สำหรับเกรด B
+        char grade3 = 'C';  // ตัวแปร char สำหรับเกรด C
+        char grade4 = 'D';  // ตัวแปร char สำหรับเกรด D
+        char grade5 = 'E';  // ตัวแปร char สำหรับเกรด E
+        
+        System.out.println("เกรดที่ประกาศทั้งหมด: " + grade1 + ", " + grade2 + ", " + grade3 + ", " + grade4 + ", " + grade5);
+        //4
+
+        float studentgradepoint = '1';
+        System.out.println("ขนาดตัวเเปร studentgradepoint = " + studentgradepoint);
+
+        //5
+        float Pi = '1';
+        System.out.println("ค่า ¶ ขนาดตัวเเปร Pi = " +Pi );
+        //6
+
+        int studentmoney = '5';
+        System.out.println("ขนาดตัวเเปร studentmoney = " + studentmoney);
+        //7
+
+        int years = '8';
+        System.out.println("ขนาดตัวเเปร  years= " + years);
+
+    
+
+
+    }
+    
+}
