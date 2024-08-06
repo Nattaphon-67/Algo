bf66742b56928d0f84f812d4e5fea93eb1a86cbf
Nattaphon-67 <674259006@webmail.npru.ac.th>
2024-08-06 12:10:04 +0700

    Nattaphon

---
+++ Error/fahren.java
@@ -0,0 +1,9 @@
+package Error;
+
+public class fahren {
+    public static void main (String args[]){
+        float fahrenheit = 80.0f;
+        float celsius = (fahrenheit = 32) * 5/9;
+        System.out.println(fahrenheit + "fahrenheit is equl to" + celsius+"Celsius");
+}
+}
